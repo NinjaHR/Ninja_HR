716d2c60e01cba0eafe26b89a5935eba1b3683dc
Ankit07kumar11 <ankitkumar56074@gmail.com>
2022-09-06 15:44:41 +0530

    dashboard

---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Dashboard = () => {
+    return (
+        <div >
+            <h1 className='dashboard' style={{display:'flex',textAlign:"center"}}>NINJA HR DASHBOARD HERE</h1>
+        </div>
+    );
+};
+
+export default Dashboard;
